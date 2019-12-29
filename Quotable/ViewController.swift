1c043ac4f027824f839a0abcd5aa114dc3e1ceeb
Stephanie Chiu <stephaniechiu@outlook.com>
2019-12-29 13:53:37 -0800

    Initial Commit

---
+++ Quotable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quotable
+//
+//  Created by Stephanie on 12/29/19.
+//  Copyright © 2019 Stephanie Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
