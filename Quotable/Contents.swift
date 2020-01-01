c05db7028470ac9b34540fd7da74e0ddd2d7f42f
Steven Yang <skewerblack@gmail.com>
2019-12-31 17:01:58 -0800

    Update the QuoteContent struct

--- Quotable/Contents.swift
+++ Quotable/Contents.swift
@@ -8,25 +8,10 @@
 
 import Foundation
 
-struct quoteContents: Codable {
+struct QuoteContents: Codable {
     struct Contents: Codable {
-        struct Content: Codable {
-            var quote: String
-            var author: String
-        }
-        var quotes: [Content]
+        var quote: String
+        var author: String
     }
-    var quoteResults: [Contents]
+    var results: [Contents]
 }
-
-//if let jsonData = jsonString.data(using: .utf8)
-//{
-//    let decoder = JSONDecoder()
-//
-//    do {
-//        let user = try decoder.decode(User.self, from: jsonData)
-//        print(user.last_name)
-//    } catch {
-//        print(error.localizedDescription)
-//    }
-//}
